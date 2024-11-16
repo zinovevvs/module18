fb97744ad5ec37b2ed87a413d301e82227b2e9ac
zinovevvs <ziryanov97@gmail.com>
2024-11-16 14:43:59 +0500

    first commit

---
+++ exampe1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Exampe1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exampe1'
---
+++ exampe2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Exampe2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exampe2'
